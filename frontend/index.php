6c2f071181cfbc7561fd4072219c39ed519d094c
saltpinna <98013379+saltpinna@users.noreply.github.com>
2022-02-08 14:53:32 +0100

    Style fixes
    Style fixes to index and user registration done in CSS file

--- frontend/index.php
+++ frontend/index.php
@@ -3,15 +3,17 @@
   <head>
     <link href="styles.css" rel="stylesheet" type="text/css" />
     <div class="header">
-    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 30px; margin-right: -10px;">CoTRACK-19<button type="submit" id="myButton" class="button button_header";>LOGIN</form><form method="get" action="user_reg.php"><button type="submit" id="myButton" class="button button_header" >REGISTER USER</form></h1>
+    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 30px; margin-right: 0px;">CoTRACK-19<button type="submit" id="myButton" class="button button_header";>LOGIN</form><form method="get" action="user_reg.php"><button type="submit" id="myButton" class="button button_header" >REGISTER USER</form></h1>
     </div>
   </head>
-  <body>
+ <body>
     <div class="container">
-      <img src="map_front_background.jpg" alt="background" style= "width: 1800px; height:820px; position:relative; top:-28px; left:-100px; right: -100px; opacity: 0.2;">
+      <!-- <img src="map_front_background.jpg" alt="background" style= "width: 108%; height:820px; padding:0; position:relative; top:-28px; left:-100px; right: -100px; opacity: 0.2;"> -->
+      <!-- <img src="map_front_background.jpg" alt="background" style= "opacity:0.2; position:relative; height:820px; top:-28px; width: 1493px; left:-10px;"> -->
+      <img src="map_front_background.jpg" class=body_frontpage>
       <div class="centered">COVID-19 Restriction Tracking System</div>
       <div class="boxed_l">blablabla</div><div class="boxed_m">Inna är en stjärt</div><div class="boxed_r">blablabla</div>
-  </body>
+</body>
 </div>
 </div>
 </html>
