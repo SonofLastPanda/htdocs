58ed660d0b2daf07c844a15315dcad383bf2ed7c
klso6609 <98013827+klso6609@users.noreply.github.com>
2022-02-08 09:23:44 +0100

    Fixed buttons
    Here the buttons actually work to navigate between sites, and the design is slightly altered. Only home page and user registration page.

--- frontend/index.php
+++ frontend/index.php
@@ -3,14 +3,14 @@
   <head>
     <link href="styles.css" rel="stylesheet" type="text/css" />
     <div class="header">
-    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 30px; margin-right: -10px;"> CoTRACK-19<button onclick="location.href = 'www.yoursite.com';" id="myButton" class="button button_header";>LOGIN</button><button onclick="location.href = 'www.yoursite.com';" id="myButton" class="button button_header">REGISTER USER</button></h1>
+    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 30px; margin-right: -10px;">CoTRACK-19<button type="submit" id="myButton" class="button button_header";>LOGIN</form><form method="get" action="user_reg.php"><button type="submit" id="myButton" class="button button_header" >REGISTER USER</form></h1>
     </div>
   </head>
   <body>
     <div class="container">
       <img src="map_front_background.jpg" alt="background" style= "width: 1800px; height:820px; position:relative; top:-28px; left:-100px; right: -100px; opacity: 0.2;">
       <div class="centered">COVID-19 Restriction Tracking System</div>
-      <div class="boxed_l">blablabla</div><div class="boxed_c";>blablabla</div><div class="boxed_r">blablabla</div>
+      <div class="boxed_l">blablabla</div><div class="boxed_m">Inna är en stjärt</div><div class="boxed_r">blablabla</div>
   </body>
 </div>
 </div>
---
+++ frontend/user_reg.php
@@ -0,0 +1,45 @@
+<!-- Code for the webpage in which one can register as a user.  -->
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="styles.css" rel="stylesheet" type="text/css"/> <!-- Connecting to the css file named styles.css -->
+    <div class="header">
+    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 30px; margin-right: -10px;">CoTRACK-19<form method="get" action="index.php"><button type="submit" id="myButton" class="button button_header";>HOME</form></h1>
+    </div>
+  </head>
+  <body>
+  <div class="container">
+    <div class="boxed_c">
+      <form action="/performreg.php">
+      <div>
+      <h1 style= "text-align: center;">User Registration</h1>
+      <p>Please fill in this form below:</p>
+      <label><b>Username:</b></label>
+      <input type="text" placeholder=" Enter Username" name="username" required><br><br>
+      <label><b>Email:</b></label>
+      <input type="text" placeholder="Enter Email" name="useremail" required><br><br>
+      <label><b>Password:</b></label>
+      <input type="password" placeholder="Enter Password" name="userpassword" required><br><br>
+      <label><b>Confirm Password:</b></label>
+      <input type="password" placeholder="Confirm Password" name="confirmpassword" required><br><br>
+      <label for="Nationality"><b>Nationality:</b></label>
+      <select id="nationality" name="nationality">
+      <option value="HongKong">Hong Kong</option>
+      <option value="India">India</option>
+      <option value="Sweden">Sweden</option>
+      <option value="Turkey">Türkiye</option>
+      </select><br><br>
+      <label><b># of Vaccine doses:</b></label>
+      <input type="vaccine_doses" placeholder="Number of Doses" name="vaccine_doses" required><br><br>
+      <label><b>Vaccine type:</b></label>
+      <input type="vaccine_type" placeholder="Brand of Vaccine" name="vaccine_type" required><br><br>
+      <p>By creating an account you agree to our <a href="linktotermsandprivacypage.html" style="color:dodgerblue">Terms & Privacy</a>.</p>
+      <div><form method="get" action="index.php"><button type = "submit" id="myButton" class="button button_register";>CANCEL</form>
+      <button onclick="location.href = 'www.yoursite.com';" id="myButton" class="button button_register"; style= "float: center;">REGISTER</button>
+      </div></div>
+      </form>
+    </div>
+  </body>
+</div>
+</div>
+</html>
