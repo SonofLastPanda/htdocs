cef0fd941e084fa6ace31c36e9e6cf63f2b306a4
Erkin Alacamli <erkinalacamli@sabanciuniv.edu>
2022-02-08 18:09:24 +0100

    Register form for backend created, not complete yet

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+##not complete
+def register(request):
+    if request.method == 'POST':
+        received_json_data = json.loads(request.body) #depends on if the request is json 
+        user = Users() #from databaseApplication.models import Users ##From the database
+        user.name = received_json_data.get('username', None)
+        user.password = received_json_data.get('password', None)
+        user.mail_adress=received_json_data.get('email', None)
+        user.save()
+        ##########################
+        profile = User() ##from django.contrib.auth.models import User  #from the database
+        profile.first_name = received_json_data.get('first_name', None)
+        profile.last_name = received_json_data.get('last_name', None)
+        profile.email = received_json_data.get('email', None)
+        profile.username = received_json_data.get('username', None)
+        profile.password = make_password(received_json_data.get('password', None))
+        profile.save()
+
+    form = UserCreationForm(request.POST)
+    return render(request,'signup.html', {'form': form})
