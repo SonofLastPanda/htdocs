98087d909bb910a3d9a68ac40a0d04dbbb0531ba
saltpinna <98013379+saltpinna@users.noreply.github.com>
2022-02-09 09:15:33 +0100

    Header looks good in all pages

--- frontend/login.php
+++ frontend/login.php
@@ -4,12 +4,12 @@
   <head>
     <link href="styles.css" rel="stylesheet" type="text/css"/> <!-- Connecting to the css file named styles.css -->
     <div class="header">
-    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 30px; margin-right: -10px;">CoTRACK-19<form method="get" action="index.php"><button type="submit" id="myButton" class="button button_header";>HOME</form></h1>
+    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 15px; margin-right: 0px;">CoTRACK-19<form method="get" action="index.php"><button type="submit" id="myButton" class="button button_header" style="margin-right:7px; top:-53px;";>HOME</form></h1>
     </div>
   </head>
   <body>
   <div class="container">
-    <div class= "boxed_c">
+    <div class= "boxed_login">
       <form action="login.php">
       <div>
       <h1 style= "text-align: center;">Login</h1>
