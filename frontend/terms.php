283139ae1d175bdd0fbb1ddb78ccc39a4531fad4
klso6609 <98013827+klso6609@users.noreply.github.com>
2022-02-09 13:16:23 +0100

    terms upload
    Addition of the terms and conditions page

---
+++ frontend/terms.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="styles.css" rel="stylesheet" type="text/css"/> <!-- Connecting to the css file named styles.css -->
+    <div class="header">
+    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 15px; margin-right: 0px;">CoTRACK-19<form method="get" action="index.php"><button type="submit" id="myButton" class="button button_header" style="margin-right:7px; top:-53px;";>HOME</form></h1>
+    </div>
+  </head>
+  <body>
+  <div class="container">
+    <div class= "boxed_login">
+      <form action="login.php">
+      <div>
+      <h1 style= "text-align: center;">Terms & Conditions</h1>
+      <p>The information on this page is not exhaustive. It is based on the latest available publicly data and authoritative national sources. CoTRACK-19 tries to keep the information on the site up to date, but we take no explicit or implied responsibility for its completeness or accuracy or for personal decisions based on the information.</p>
+      </div>
+      </div>
+      </form>
+    </div>
+  </div>
+  </body>
+</div>
+</div>
+</html>
