80c7101f11b9e8e523d8c3009960233c84717d9f
Erkin Alacamli <41624456+SonofLastPanda@users.noreply.github.com>
2022-02-16 16:31:29 +0100

    Delete backend directory

--- backend/databaseapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Users
-# Register your models here.
-
-admin.site.register(Users)
--- backend/databaseapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatabaseappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'databaseapp'
