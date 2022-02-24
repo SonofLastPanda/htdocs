14d7e95c920fae907a1d286233657ced8ba2ceda
saltpinna <98013379+saltpinna@users.noreply.github.com>
2022-02-24 13:06:16 +0100

    Small fixes

--- frontend/connect.php
+++ frontend/connect.php
@@ -1,14 +1,11 @@
 <?php
-$hostname2 = 'localhost';
-$username2 = "root";
-$password2 = "root";
-$dbname2 = "batgirl_project";
-
-//Establishing connection
-$link = mysqli_connect($hostname2, $username2, $password2, $dbname2);
-
-if (!$link) { //Error message if no connection to database
-echo "Error: Unable to connect to MySQL." . mysqli_connect_error() . PHP_EOL;
-exit;
-}
+    $hostname = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "BATGIRL_PROJECT";
+    $link = mysqli_connect($hostname, $username, $password, $dbname);
+    if (!$link) {
+        echo "Error: Unable to connect to MySQL." . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
 ?>
