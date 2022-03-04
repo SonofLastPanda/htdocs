1945be3b1fd2efe76f9e7ace4cf5f1c318e26f3f
ESiggstedt <78079034+ESiggstedt@users.noreply.github.com>
2022-03-04 13:34:12 +0100

    change encryption key

--- frontend/performreg.php
+++ frontend/performreg.php
@@ -11,7 +11,7 @@
 <?php
     // establish server connection
     define("encryption_method", "AES-128-CBC");
-    define("key", "your_amazing_key_here"); //change the encryption key?? The web site says one should
+    define("key", "batgirl_to_the_rescue");
     include 'connect.php';
 
     //encrypt data    
