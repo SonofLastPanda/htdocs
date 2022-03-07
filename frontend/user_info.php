8435ee34f83686c6704f2b6b81659f9e0a6a700f
saltpinna <98013379+saltpinna@users.noreply.github.com>
2022-03-07 15:40:38 +0100

    Added log out button

--- frontend/user_info.php
+++ frontend/user_info.php
@@ -39,7 +39,7 @@ $result=mysqli_query($link, "SELECT users.email, users.password FROM users WHERE
   <head>
     <link href="styles.css" rel="stylesheet" type="text/css"/> <!-- Connecting to the css file named styles.css -->
     <div class="header">
-    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 15px; margin-right: 0px;">CoTRACK-19<form method="get" action="user_frontpage.php"><button type="submit" id="myButton" class="button button_header" style="margin-right:7px; top:-53px;";>HOME</form></h1>
+    <h1><img src="logo1.2.png" alt="BatGirl logo" width="50" height="50" style= "margin-bottom: -10px; margin-left: 15px; margin-right: 0px;">CoTRACK-19<form method="get" action="logout.php"><button type="submit" id="myButton" class="button button_header" style="margin-right:7px; top:-53px;";>LOG OUT</form><form method="get" action="user_frontpage.php"><button type="submit" id="myButton" class="button button_header" style="margin-right:7px; top:-53px;";>HOME</form></h1>
     </div>
   </head>
   <body>
