e8bab447114e2a2153598925911748690be56e2e
asgmaetrix <98015303+asgmaetrix@users.noreply.github.com>
2022-03-10 11:11:45 +0100

    Update user_reg.php

--- frontend/user_reg.php
+++ frontend/user_reg.php
@@ -64,6 +64,23 @@
         }
         return true;
     }
+ function ValidateEmail()
+{
+var inputText=document.getElementById("useremail").value;
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
 </script>
 </html>
 <!-- style="position: relative; top: -45px; font-family: 'Red Hat Display';" -->
